ed66876890466bed691e201e97669e66ad4ea3a1
vermad843 <vishnuverma7524@gmail.com>
2019-11-08 04:29:10 +0530

    working app

---
+++ src/MovieList.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import MovieCard from './MovieCard';
+import axios from 'axios';
+import './App.css';
+
+
+
+class MoviesList extends React.Component {
+    state = {
+        moviesList: ['tt2294629'],
+        searchTerm: ''
+    };
+
+    search = event => {
+        event.preventDefault();
+        axios
+            .get(
+                `https://www.omdbapi.com/?&s=${
+                    this.state.searchTerm
+                }&plot=full`
+            )
+            .then(res => res.data)
+            .then(res => {
+                if (!res.Search) {
+                    this.setState({ moviesList: [] });
+                    return;
+                }
+
+                const moviesList = res.Search.map(movie => movie.imdbID);
+                this.setState({
+                    moviesList
+                });
+            });
+    };
+
+    handleChange = event => {
+        this.setState({
+            searchTerm: event.target.value
+        });
+    };
+
+    render() {
+        const { moviesList } = this.state;
+
+        return (
+            <div>
+                <form onSubmit={this.search}>
+                    <input
+                        placeholder="Search for a movie"
+                        onChange={this.handleChange}
+                    />
+                    <button type="submit">Go
+                        <i className="fa fa-search" />
+                    </button>
+                </form>
+                {moviesList.length > 0 ? (
+                    moviesList.map(movie => (
+                        <MovieCard movieID={movie} key={movie} />
+                    ))
+                ) : (
+                    <p>
+                         Movie Not Found!!!
+                    </p>
+                )}
+            </div>
+        );
+    }
+}
+
+
+
+export default MoviesList;
