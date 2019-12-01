d6f0e13a1ea3506ecbe87e56e2a7af1ea1c481a1
vermad843 <vishnuverma7524@gmail.com>
2019-12-01 19:58:47 +0530

    Movie Search App

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,71 +1,85 @@
-import React from 'react';
-import MovieCard from './MovieCard';
-import axios from 'axios';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import MovieCard from './MovieCard'
 
 const API_KEY = process.env.REACT_APP_MOVIE_API_KEY
-class MoviesList extends React.Component {
-    state = {
-        moviesList: ['tt2294629'],
-        searchTerm: ''
+class MovieList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            moviesList: ['tt2294629'],
+            searchTerm: '',
+            title : 'Movie Search App',
+            loading : false ,
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.formSubmitted = this.formSubmitted.bind(this);
+
+    }
+
+    formSubmitted(event) {
+      event.preventDefault();
+      this.setState({loading : true})
+      axios
+      .get(
+        `https://www.omdbapi.com/?apikey=${API_KEY}&s=${
+            this.state.searchTerm
+        }&plot=full`
+    )
+    .then(res => res.data)
+    .then(res => {
+        this.setState({
+            searchTerm : '',
+            loading : false
+        })
+        console.log(res)
+        if (!res.Search) {
+            this.setState({ moviesList: [], 
+                        });
+            return;
+        }
+
+        const moviesList = res.Search.map(movie => movie.imdbID);
+        this.setState({
+            moviesList,
+        });
+     });
     };
 
-    search = event => {
-        event.preventDefault();
-        axios
-            .get(
-                `https://www.omdbapi.com/?apikey=${API_KEY}&s=${
-                    this.state.searchTerm
-                }&plot=full`
-            )
-            .then(res => res.data)
-            .then(res => {
-                if (!res.Search) {
-                    this.setState({ moviesList: [] });
-                    return;
-                }
 
-                const moviesList = res.Search.map(movie => movie.imdbID);
-                this.setState({
-                    moviesList
-                });
-            });
-    };
-
-    handleChange = event => {
+    handleChange(event) {
         this.setState({
             searchTerm: event.target.value
         });
     };
-
     render() {
-        const { moviesList } = this.state;
-
+        const {title, moviesList, loading } = this.state;
         return (
             <div>
-                <form onSubmit={this.search}>
-                    <input
-                        placeholder="Search for a movie"
-                        onChange={this.handleChange}
-                    />
-                    <button type="submit">Go
-                        <i className="fa fa-search" />
-                    </button>
-                </form>
-                {moviesList.length > 0 ? (
-                    moviesList.map(movie => (
-                        <MovieCard movieID={movie} key={movie} />
-                    ))
-                ) : (
-                    <p>
-                         Movie Not Found!!!
-                    </p>
-                )}
+                {loading  ? <img style = {{textAlign : "center"}} src = "Facebook-1s-200px.gif" alt = "loading" /> : 
+                <form onSubmit = {this.formSubmitted}>
+                    <h1>{title}</h1>
+                    <input 
+                     onChange = {this.handleChange}
+                     placeholder = "Search for a movie"
+                     className = "u-full-width"
+                     type = "text" 
+                     />
+                     <button type = "submit">Go</button>
+                </form>} 
+                 {moviesList.length > 0 ? (
+                     moviesList.map(movie => (
+                         <MovieCard movieID = {movie} key = {movie}/>
+                     ))
+                 ) : (
+                   <p>
+                       Movie Not Found!!!
+                   </p>     
+                 )}  
             </div>
         );
     }
 }
 
-
-
-export default MoviesList;
+export default MovieList;
